386014c57f07cdcadb9359880f2e005715f3ae8f
Rafael Ríos Moya <rafael@fintonic.com>
2018-04-14 12:55:43 +0200

    Add lombok support for configuration properties. Documentation improved.

--- arangodb-spring-boot-autoconfigure/src/main/java/io/github/ganchix/arangodb/properties/ArangoProperties.java
+++ arangodb-spring-boot-autoconfigure/src/main/java/io/github/ganchix/arangodb/properties/ArangoProperties.java
@@ -3,6 +3,8 @@ package io.github.ganchix.arangodb.properties;
 import com.arangodb.Protocol;
 import com.arangodb.entity.LoadBalancingStrategy;
 import com.arangodb.internal.ArangoDBConstants;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
@@ -12,6 +14,8 @@ import java.util.List;
  *
  * Created by Rafael Ríos on 8/04/18.
  */
+@Getter
+@Setter
 @ConfigurationProperties(prefix = "spring.data.arangodb")
 public class ArangoProperties {
 
@@ -21,22 +25,22 @@ public class ArangoProperties {
     private List<String> hosts;
 
     /**
-     * ArangoDB host, {@link ArangoDBConstants default value}
+     * ArangoDB host, 127.0.0.1 default value
      */
     private String host = ArangoDBConstants.DEFAULT_HOST;
 
     /**
-     * ArangoDB port, {@link ArangoDBConstants default value}
+     * ArangoDB port, 8529 default value
      */
     private Integer port = ArangoDBConstants.DEFAULT_PORT;
 
     /**
-     * Socket connect timeout(millisecond), {@link ArangoDBConstants default value}
+     * Socket connect timeout(millisecond), 0 default value
      */
     private Integer timeout = ArangoDBConstants.DEFAULT_TIMEOUT;
 
     /**
-     * Basic Authentication User, {@link ArangoDBConstants default value}
+     * Basic Authentication User, root default value
      */
     private String user = ArangoDBConstants.DEFAULT_USER;
 
@@ -46,7 +50,7 @@ public class ArangoProperties {
     private String password;
 
     /**
-     * Use SSL connection, {@link ArangoDBConstants default value}
+     * Use SSL connection, false default value
      */
     private Boolean useSsl = ArangoDBConstants.DEFAULT_USE_SSL;
 
@@ -71,117 +75,13 @@ public class ArangoProperties {
     private Boolean acquireHostList = ArangoDBConstants.DEFAULT_ACQUIRE_HOST_LIST;
 
     /**
-     * Protocol used, {@link ArangoDBConstants default value}
+     * Protocol used, VST default value
      */
     private Protocol protocol = ArangoDBConstants.DEFAULT_NETWORK_PROTOCOL;
 
     /**
-     * Load balancing strategy used, {@link ArangoDBConstants default value}
+     * Load balancing strategy used, NONE default value
      */
     private LoadBalancingStrategy loadBalancingStrategy = ArangoDBConstants.DEFAULT_LOAD_BALANCING_STRATEGY;
 
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getUser() {
-        return user;
-    }
-
-    public void setUser(String user) {
-        this.user = user;
-    }
-
-    public Integer getTimeout() {
-        return timeout;
-    }
-
-    public void setTimeout(Integer timeout) {
-        this.timeout = timeout;
-    }
-
-    public List<String> getHosts() {
-        return hosts;
-    }
-
-    public void setHosts(List<String> hosts) {
-        this.hosts = hosts;
-    }
-
-    public Boolean getUseSsl() {
-        return useSsl;
-    }
-
-    public void setUseSsl(Boolean useSsl) {
-        this.useSsl = useSsl;
-    }
-
-    public Integer getChunksize() {
-        return chunksize;
-    }
-
-    public void setChunksize(Integer chunksize) {
-        this.chunksize = chunksize;
-    }
-
-    public Integer getMaxConnections() {
-        return maxConnections;
-    }
-
-    public void setMaxConnections(Integer maxConnections) {
-        this.maxConnections = maxConnections;
-    }
-
-    public String getDatabaseName() {
-        return databaseName;
-    }
-
-    public void setDatabaseName(String databaseName) {
-        this.databaseName = databaseName;
-    }
-
-    public String getHost() {
-        return host;
-    }
-
-    public void setHost(String host) {
-        this.host = host;
-    }
-
-    public Integer getPort() {
-        return port;
-    }
-
-    public void setPort(Integer port) {
-        this.port = port;
-    }
-
-    public Boolean getAcquireHostList() {
-        return acquireHostList;
-    }
-
-    public void setAcquireHostList(Boolean acquireHostList) {
-        this.acquireHostList = acquireHostList;
-    }
-
-    public Protocol getProtocol() {
-        return protocol;
-    }
-
-    public void setProtocol(Protocol protocol) {
-        this.protocol = protocol;
-    }
-
-    public LoadBalancingStrategy getLoadBalancingStrategy() {
-        return loadBalancingStrategy;
-    }
-
-    public void setLoadBalancingStrategy(LoadBalancingStrategy loadBalancingStrategy) {
-        this.loadBalancingStrategy = loadBalancingStrategy;
-    }
 }
