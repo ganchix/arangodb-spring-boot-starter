f07272183ee20772ce799d6cd224fe67f23b0cd9
rafa.rios <rafa.rios@2getherbank.com>
2018-05-18 16:05:01 +0200

    Start with testing

---
+++ arangodb-spring-boot-autoconfigure/src/test/java/io/github/ganchix/arangodb/configuration/ArangodbAutoConfigurationTests.java
@@ -0,0 +1,36 @@
+package io.github.ganchix.arangodb.configuration;
+
+import com.arangodb.springframework.core.ArangoOperations;
+import io.github.ganchix.arangodb.ArangoDBContainer;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.springframework.boot.autoconfigure.AutoConfigurations;
+import org.springframework.boot.test.context.runner.ApplicationContextRunner;
+import org.testcontainers.containers.GenericContainer;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertNotNull;
+
+public class ArangodbAutoConfigurationTests {
+
+    private final ApplicationContextRunner contextRunner = new ApplicationContextRunner()
+            .withConfiguration(AutoConfigurations.of(ArangoAutoConfiguration.class));
+
+    @Rule
+    public ArangoDBContainer arangoDBContainer = new ArangoDBContainer().withPassword("openSesame");
+
+
+
+    @Test
+    public void checkIfArangoDBIsConnected() {
+        this.contextRunner
+                .withPropertyValues("spring.data.arangodb.password=openSesame",
+                        "spring.data.arangodb.port="+arangoDBContainer.getMappedPort(arangoDBContainer.getPort()))
+                .run((context) -> {
+                    ArangoOperations arangoOperations = context.getBean(ArangoOperations.class);
+                    assertNotNull(arangoOperations.getVersion());
+                    assertThat(context).hasSingleBean(ArangoOperations.class);
+                });
+    }
+}
